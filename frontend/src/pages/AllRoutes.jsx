a4f7e5030ac14c48338cf5ad16b8b364832e2ca0
Shiv Pratap Singh Yadav <shivpratapsinghyadav38@gmail.com>
2023-01-20 01:40:08 +0530

    shiv day-3 commit

--- frontend/src/pages/AllRoutes.jsx
+++ frontend/src/pages/AllRoutes.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {Route,Routes} from 'react-router-dom'
+import Cart from './Cart'
 import Home from './Home'
 import HomedataSinglePage from './HomedataSinglePage'
+import MenPage from './MenPage'
 const AllRoutes = () => {
   return (
     <>
 <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/product/:id" element={<HomedataSinglePage/>}/>
+    <Route path="/cart" element={<Cart/>}/>
+    <Route path="/mens" element={<MenPage/>}/>
 </Routes>
     </>
   )
--- frontend/src/pages/HomedataSinglePage.jsx
+++ frontend/src/pages/HomedataSinglePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Box,Text,Image} from '@chakra-ui/react'
+import {Box,Button,Text,Image, Heading} from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
 import { useSelector } from "react-redux";
 import {useEffect, useState} from "react";
 import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getProductData } from '../redux/app/action';
+const disImg="https://i.postimg.cc/Kzk9hV5x/discount-3.png";
 const HomedataSinglePage = () => {
     const{id}=useParams();
     const products=useSelector((store)=>store.product);
@@ -14,7 +15,7 @@ const HomedataSinglePage = () => {
     
    useEffect(()=>{
     if(products.length===0){
-        dispatch(getProductData())
+        dispatch(getProductData)
     }
    },[products.length,dispatch])
 
@@ -26,11 +27,43 @@ const HomedataSinglePage = () => {
    })
   return (
     <>
-     {/* <Navbar/> */}
-     <Box>
-        <Image src={currentProduct.images}/>
-        <Text>{currentProduct.title}</Text>
-        <Text>₹{currentProduct.original_price}</Text>
+     <Navbar/>
+     
+     <Box display={["block","","flex","flex"]} pt="100px" w="75%" m="auto" mt="7%" border="px solid red">
+       <Box w="50%" border="px solid red">
+        <Image border="px solid red" display="block" m="auto" src={currentProduct.image}/>
+        <Box  display={["grid","","","flex"]} gap={["10px","","",""]} w="70%" m="auto" mt="20%" border="px solid red">
+       <Link to="/cart"><Button  w={["82%","","","95%"]} fontSize={["20px","","","20px"]} border="1px solid black" _hover={{bgColor:'white'}} bgColor="white" >{`🛒`}Add To Cart</Button></Link> 
+        <Button  w={["82%","","","50%"]} fontSize={["20px","","","20px"]}  bgColor="rgb(244,51,151)" color="white" _hover={{bgColor:'rgb(244,51,151)'}}>{`>>`}Buy Now</Button>
+        </Box>
+        <Box  display={["flex","","","flex"]} gap="10px" justifyContent="center">
+        <Image  w={["25%","","","10%"]} m="right" mt="5%" src={currentProduct.image}/>
+        <Image w={["25%","","","10%"]} m="right" mt="5%" src={currentProduct.image}/>
+        </Box>
+       </Box>
+       <Box w="50%" border="px solid red">
+        <Text mt="5px" ml="10px" fontSize="18px" color="rgb(153, 153, 153)">{currentProduct.title}</Text>
+        <Box display="flex" alignItems="center">
+                      <Text ml="10px" color="black" fontSize="27px" fontWeight="700" >₹{currentProduct.price}</Text>
+                      <del style={{display:"inline", marginLeft:"5px",  color:"rgb(153, 153, 153)"}}>₹{currentProduct.mrp}</del> 
+                      <Text ml="5px" fontWeight="700" color="rgb(35,187,117)">{currentProduct.discount}% off</Text> 
+                      </Box>
+                      <Image ml="10px" w="16px" h="16px" display="inline" src={disImg}/>
+                      <Text  style={{display:"inline",fontSize:"16px", color:" color: rgb(140, 140, 140);", marginLeft:"10px"}}>₹ 100 discount on 1st order</Text>
+                      <Text  style={{fontSize:"16px", color:" color: rgb(140, 140, 140);", marginLeft:"12px"}}>Favorite</Text>
+                      <Button  ml="10px" borderRadius="50px" h="22px" w={["","","","22%"]} color="rgb(153, 153, 153)">Free Delivery</Button><br/>
+                      <Button  _hover={{bgColor:'rgb(35,187,117)'}} ml="10px" mt="15px" borderRadius="50px" bgColor="rgb(35,187,117)" color="white">3.6⭐</Button>
+
+                      <Box mt="8%">
+                        <Heading color="black">Product Details</Heading>
+                        <Text color="rgb(153, 153, 153)" ml="8px">Item : Good ✔</Text>
+                        <Text color="rgb(153, 153, 153)" ml="8px">Quality : Excellent ⭐</Text>
+                        <Text color="rgb(153, 153, 153)" ml="8px">Multipacks</Text>
+                        <Text color="rgb(153, 153, 153)" ml="8px">Good quality Product made of very fine quality material. This Product is long lasting.</Text>
+                        <Text color="rgb(153, 153, 153)" ml="8px">Country of Origin : India 🌐</Text>
+                        <Text color="rgb(153, 153, 153)" ml="8px">More Information...</Text>
+                      </Box>
+       </Box>
      </Box>
    </>
     
